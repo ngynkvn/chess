84773527cde58b6ccd5ded7849f4cf48a83682d4
Carl Lin <lincarl@ufl.edu>
2018-04-17 23:54:58 -0400

    Updated UCI

---
+++ include/uci.h
@@ -0,0 +1,25 @@
+#ifndef UCI_H
+#define UCI_H
+
+#include "Move.h"
+#include "Piece.h"
+#include "Board.h"
+#include <vector>
+
+class UCI
+{
+public:
+    UCI();
+    static void newUCI();
+    static void inputIsReady();
+    void debug();
+    void setOption(string);
+    void uciNewGame();
+    void inputIsGo();
+    void print();
+private:
+    string nameOfEngine;
+    string Line;
+};
+
+#endif
