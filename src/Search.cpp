6fde6ab97f1d3014f156718077c4132f86d438a3
mhluo132 <luo.michael@gmail.com>
2018-04-20 16:39:25 -0400

    Cleaned up redundant code

--- src/Search.cpp
+++ src/Search.cpp
@@ -4,7 +4,17 @@
 namespace Search
 {
 
-std::vector<Coord> dirQueen = {Coord(1, 1),
+std::vector<Coord> dirRook = {Coord(0, 1),          //The 4 Ray directions rooks can slide to (raymove)
+                              Coord(0, -1),
+                              Coord(1, 0),
+                              Coord(-1, 0)};
+
+std::vector<Coord> dirBishop = {Coord(1, 1),        //The 4 Ray directions bishops can slide to (raymove)
+                                Coord(1, -1),
+                                Coord(-1, -1),
+                                Coord(-1, 1)};
+
+std::vector<Coord> dirQueen = {Coord(1, 1),       //The 8 Ray directions queens can slide to (raymove)
                                Coord(1, -1),
                                Coord(-1, -1),
                                Coord(-1, 1),
@@ -13,21 +23,7 @@ std::vector<Coord> dirQueen = {Coord(1, 1),
                                Coord(1, 0),
                                Coord(-1, 0)};
 
-std::vector<Coord> dirBishop = {Coord(1, 1),
-                                Coord(1, -1),
-                                Coord(-1, -1),
-                                Coord(-1, 1)};
-
-std::vector<Coord> dirWPawnM = {Coord(0, 1)};
-std::vector<Coord> dirWPawnC = {Coord(1, 1),
-                                Coord(-1, 1)};
-std::vector<Coord> dirWPawnS = {Coord(0, 2)};
-std::vector<Coord> dirBPawnM = {Coord(0, -1)};
-std::vector<Coord> dirBPawnC = {Coord(1, -1),
-                                Coord(-1, -1)};
-std::vector<Coord> dirBPawnS = {Coord(0, -2)};
-
-std::vector<Coord> dirKnight = {Coord(1, 2),
+std::vector<Coord> dirKnight = {Coord(1, 2),        //The 8 square directions a knight can jump to (squareMove)
                                 Coord(2, 1),
                                 Coord(2, -1),
                                 Coord(1, -2),
@@ -36,12 +32,7 @@ std::vector<Coord> dirKnight = {Coord(1, 2),
                                 Coord(-2, -1),
                                 Coord(-1, -2)};
 
-std::vector<Coord> dirRook = {Coord(0, 1),
-                              Coord(0, -1),
-                              Coord(1, 0),
-                              Coord(-1, 0)};
-
-std::vector<Coord> dirKing = {Coord(-1, -1),
+std::vector<Coord> dirKing = {Coord(-1, -1),        //The 8 square directions a king can slide to (squareMove)
                               Coord(-1, 0),
                               Coord(-1, 1),
                               Coord(0, 1),
@@ -50,51 +41,18 @@ std::vector<Coord> dirKing = {Coord(-1, -1),
                               Coord(1, 0),
                               Coord(1, 1)};
 
-void rayMove(std::vector<Move> &v, Board b, std::vector<Coord> &pieceV, std::vector<Coord> dir)
-{
-    Coord ctr;
+//The three unique movements of a white pawn (Push, Capture, and Move2)
+std::vector<Coord> dirWpawnPush = {Coord(0, 1)};
+std::vector<Coord> dirWpawnCapture = {Coord(1, 1),
+                                    Coord(-1, 1)};
+std::vector<Coord> dirWpawnMove2 = {Coord(0, 2)}; //White Pawn on a starting rank has option to move 2 squares
 
-    for (unsigned int i = 0; i < pieceV.size(); i++)
-    {
-        Coord piece = pieceV[i];
-        for (unsigned int j = 0; j < dir.size(); j++) // the j< expression is not correct. should be j<dir.size()
-        {
-            ctr = dir[j];
-            Coord possibleMove = piece + dir[j];
-            while (b.inside(possibleMove) && (b.getPiece(possibleMove).empty() || b.getPiece(possibleMove).getColor() == b.opposite()) && b.getPiece(possibleMove).getColor() != b.same())
-            {
-                if (b.getPiece(possibleMove).getColor() == b.opposite())
-                {
-                    v.push_back(Move(piece, possibleMove));
-                    break;
-                }
-                else
-                {
-                    v.push_back(Move(piece, possibleMove));
-                    possibleMove = possibleMove + ctr;
-                }
-            }
-        }
-    }
-}
+//The three unique movements of a black pawn (Push, Capture, and Move2)
+std::vector<Coord> dirBpawnPush = {Coord(0, -1)};
+std::vector<Coord> dirBpawnCapture = {Coord(1, -1),
+                                    Coord(-1, -1)};
+std::vector<Coord> dirBpawnMove2 = {Coord(0, -2)};  //Black Pawn on a starting rank has option to move 2 squares
 
-void singleMove(std::vector<Move> &v, Board b, std::vector<Coord> &pieceV, std::vector<Coord> dir)
-{
-    for (unsigned int i = 0; i < pieceV.size(); i++)
-    {
-        Coord piece = pieceV[i];
-        for (unsigned int j = 0; j < 8; j++)
-        {
-            Coord possibleMove = piece + dir[j];
-            if (b.inside(possibleMove) && (b.getPiece(possibleMove).empty() || b.getPiece(possibleMove).getColor() == b.opposite()))
-            {
-                v.push_back(Move(piece, possibleMove));
-            }
-        }
-    }
-}
-
-/*
 bool inCheck(Board b)
 {
     Coord pieceV;
@@ -108,15 +66,97 @@ bool inCheck(Board b)
         Coord possibleMove = pieceV + dirQueen[i];
         if (b.inside(possibleMove) && (b.getPiece(possibleMove).empty() || b.getPiece(possibleMove).getColor() == b.opposite()))
         {
-            v.push_back(Move(piece, possibleMove));
+            
         }
     }
 }
-*/
-std::vector<Coord> findPieces(const Board &b, ePieceCode piece)
+
+
+//Purpose: Add all possible moves to v for pieces that move in rays (Rooks, Bishops, Queens)
+//rayMove pieces can "slide" (move) in directions given by dir until square is occupied.
+void rayMove(std::vector<Move> &v, Board b, std::vector<Coord> &pieceV, std::vector<Coord> dir)
+{
+    for (unsigned int i = 0; i < pieceV.size(); i++)
+    {
+        Coord piece = pieceV[i]; //storing location
+        for (unsigned int j = 0; j < dir.size(); j++)
+        {
+            Coord possibleMove = piece + dir[j];
+
+            //Examines each square in the direction dir[j]
+            //while the square is empty or opposite (color)
+            //Note: When the possibleMove is occupied by same color piece. break while loop (do not examine rest of the squares in that direction)
+            while (b.inside(possibleMove) && (b.getPiece(possibleMove).empty() || b.getPiece(possibleMove).getColor() == b.opposite()) && inCheck(piece))
+            {
+                //if square is occupied by enemy, option to occupy square. break while loop
+                if (b.getPiece(possibleMove).getColor() == b.opposite())
+                {
+                    v.push_back(Move(piece, possibleMove));
+                    break;
+                }
+                //else add possibleMove and slide to the next square in the same direction
+                else
+                {
+                    v.push_back(Move(piece, possibleMove));
+                    possibleMove = possibleMove + dir[j];
+                }
+            }
+        }
+    }
+}
+
+//Purpose: Add all possible moves to v for pieces that move to a single square in a direction
+//squareMove pieces can move to only the first square for each direction ray
+void squareMove(std::vector<Move> &v, Board b, std::vector<Coord> &pieceV, std::vector<Coord> dir)
+{
+    for (unsigned int i = 0; i < pieceV.size(); i++)
+    {
+        Coord piece = pieceV[i]; //storing location
+        for (unsigned int j = 0; j < 8; j++)
+        {
+            Coord possibleMove = piece + dir[j];
+            if (b.inside(possibleMove) && (b.getPiece(possibleMove).empty() || b.getPiece(possibleMove).getColor() == b.opposite()))
+                v.push_back(Move(piece, possibleMove)); //if square is empty or occupied by enemy, add possibleMove to v
+        }
+    }
+}
+
+//Purpose: adds to v by generating all moves based off 3 conditional types of movements
+void pawnMove(std::vector<Move> &v, Board b, std::vector<Coord> &pieceV, std::vector<Coord> push, std::vector<Coord> move2, std::vector<Coord> dir3)
+{
+    for (unsigned int i = 0; i < pieceV.size(); i++)
+    {
+        Coord piece = pieceV[i];
+        Coord possibleMove = piece + push[0]; //possibleMove set to a pawn push
+
+        //Checks forward square for pawn push direction and checks for special 2 square move
+        if (b.inside(possibleMove) && (b.getPiece(possibleMove).empty())) //tests a pawn push
+        {
+            v.push_back(Move(piece, possibleMove));
+
+            possibleMove = piece + move2[0]; //possibleMove set to special 2 square move
+            if ((piece.y == (b.isWhite() ? 1 : 6)) && b.getPiece(possibleMove).empty()) // ???? KEVINNNN
+                v.push_back(Move(piece, possibleMove));
+        }
+
+        //for loop for the two capture directions
+        for(int j=0; j<2; j++)
+        {
+            possibleMove = piece + dir3[j]; //possibleMove set to a pawn capture direction
+            if(b.inside(possibleMove)&&b.getPiece(possibleMove).getColor() == b.opposite()) // test opposite color piece in square
+                v.push_back(Move(piece,possibleMove));
+        }
+    }
+}
+
+
+// returns a vector<Coord> of all pieces with same ePieceCode
+std::vector<Coord> findPieces(Board b, ePieceCode piece)
 {
     Piece **board = b.getBoard();
     std::vector<Coord> pieces;
+
+    //Searches all squares
     for (int i = 0; i < 8; i++)
     {
         for (int j = 0; j < 8; j++)
@@ -130,111 +170,51 @@ std::vector<Coord> findPieces(const Board &b, ePieceCode piece)
     return pieces;
 }
 
-//Dumbly adds all possible moves for King without considering castling and whether the move will place him in check.
-void kingMoves(std::vector<Move> &v, const Board &b)
+void generateMove(std::vector<Move> &v, Board b, int code)
 {
     std::vector<Coord> pieceV;
-    if (b.isWhite())
-        pieceV = findPieces(b, epcWking);
-    else
-        pieceV = findPieces(b, epcBrook);
-    singleMove(v, b, pieceV, dirKing);
-}
 
-void pawnMove(std::vector<Move> &v, Board b, std::vector<Coord> &pieceV, std::vector<Coord> dir1, std::vector<Coord> dir2, std::vector<Coord> dir3)
-{
-    Piece **board = b.getBoard();
-    Piece p;
-    for (unsigned int i = 0; i < pieceV.size(); i++)
+    // sets pieceV to the correct color
+    if (b.isWhite())
+        pieceV = findPieces(b, (ePieceCode)code); //cast code to correspoding white ePieceCode
+    else
+        pieceV = findPieces(b, (ePieceCode)(code+7)); //cast code to correspoding black ePieceCode
+
+    //Based on the ePieceCode, the appropriate move function is called with appropriate parameters
+    switch(code)
     {
-        Coord piece = pieceV[i];
-        p = board[piece.y][piece.x];
-        Coord possibleMove = piece + dir1[0];
-        if (b.inside(possibleMove) && (b.getPiece(possibleMove).empty()))
-        {
-            v.push_back(Move(piece, possibleMove));
-            possibleMove = piece + dir2[0];
-            if ((piece.y == (b.isWhite() ? 1 : 6)) && b.getPiece(possibleMove).empty())
-            {
-                v.push_back(Move(piece, possibleMove));
-            }
-        }
+        //each move function parameters are
+        //all possible moves vector v, the Board b, a vector of all piece types, and vector direction the piece moves
+        case 1: //Pawn move
+            if(b.isWhite())
+                {pawnMove(v, b, pieceV, dirWpawnPush, dirWpawnMove2, dirWpawnCapture); break;}
+            else
+                {pawnMove(v, b, pieceV, dirBpawnPush, dirBpawnMove2, dirBpawnCapture); break;}
+        case 2://Knight move
+            squareMove(v, b, pieceV, dirKnight); break;
+        case 3://Bishop move
+            rayMove(v, b, pieceV, dirBishop); break;
+        case 4://Rook move
+            rayMove(v, b, pieceV, dirRook); break;
+        case 5://Queen move
+            rayMove(v, b, pieceV, dirQueen); break;
+        case 6://king move
+            squareMove(v, b, pieceV, dirKing); break;
 
-        for(int j=0; j<2; j++)
-        {
-            possibleMove = piece + dir3[j];
-            if(b.inside(possibleMove)&&b.getPiece(possibleMove).getColor() == b.opposite())
-            {
-                v.push_back(Move(piece,possibleMove));
-            }
-        }
     }
-}
-void pawnMoves(std::vector<Move> &v, Board b)
-{
-    std::vector<Coord> pieceV;
-    if (b.isWhite())
-    {
-        pieceV = findPieces(b, epcWpawn);
-        pawnMove(v, b, pieceV, dirWPawnM, dirWPawnS, dirWPawnC);
-    }
-    else
-    {
-        pieceV = findPieces(b, epcBpawn);
-        pawnMove(v, b, pieceV, dirBPawnM, dirBPawnS, dirBPawnC);
-    }
-}
-
-void knightMoves(std::vector<Move> &v, Board b)
-{
-    std::vector<Coord> pieceV;
-    if (b.isWhite())
-        pieceV = findPieces(b, epcWknight);
-    else
-        pieceV = findPieces(b, epcBknight);
-    singleMove(v, b, pieceV, dirKnight);
-}
-
-void rookMoves(std::vector<Move> &v, Board b)
-{
-    std::vector<Coord> pieceV;
-    if (b.isWhite())
-        pieceV = findPieces(b, epcWrook);
-    else
-        pieceV = findPieces(b, epcBrook);
-    rayMove(v, b, pieceV, dirRook);
-}
-
-void bishopMoves(std::vector<Move> &v, Board b)
-{
-    std::vector<Coord> pieceV;
-    if (b.isWhite())
-        pieceV = findPieces(b, epcWbishop);
-    else
-        pieceV = findPieces(b, epcBbishop);
-    rayMove(v, b, pieceV, dirBishop);
-}
-
-void queenMoves(std::vector<Move> &v, Board b)
-{
-    std::vector<Coord> pieceV;
-    if (b.isWhite())
-        pieceV = findPieces(b, epcWqueen);
-    else
-        pieceV = findPieces(b, epcBqueen);
-    rayMove(v, b, pieceV, dirQueen);
 }
 
 std::vector<Move> generateMoveList(const Board &b)
 {
     //std::cout << "I MAKE THE GIRLS GO LIKE";
     std::vector<Move> v;
-    pawnMoves(v, b);
-    knightMoves(v, b);
-    rookMoves(v, b);
-    kingMoves(v, b);
-    bishopMoves(v, b);
-    queenMoves(v, b);
+
+    //generates all the possible moves for the code's ePieceCode representation
+    //1-6 of enum ePieceCode are piece types with a corresponding number
+    for(int code=1; code<7; code++)
+    {
+        generateMove(v, b, code);
+    }
     return v;
 }
 }
